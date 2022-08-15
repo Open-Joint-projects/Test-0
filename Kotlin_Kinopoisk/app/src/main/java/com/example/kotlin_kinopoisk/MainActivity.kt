63535eeab6e3fa83a37b5bdfc77eb32fc57994c1
WhitliyJ <shepelev.yaros1@gmail.com>
2022-08-15 21:07:47 +0300

    new commit2

--- Kotlin_Kinopoisk/app/src/main/java/com/example/kotlin_kinopoisk/MainActivity.kt
+++ Kotlin_Kinopoisk/app/src/main/java/com/example/kotlin_kinopoisk/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.kotlin_kinopoisk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.textview)
+        text.setText("layout")
 
     }
 }
