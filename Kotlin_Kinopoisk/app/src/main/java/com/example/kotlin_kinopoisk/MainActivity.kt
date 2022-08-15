a99ff58d0fd2e34df8707a2da0288b6959a1570a
WhitliyJ <shepelev.yaros1@gmail.com>
2022-08-15 21:16:25 +0300

    new commit3

--- Kotlin_Kinopoisk/app/src/main/java/com/example/kotlin_kinopoisk/MainActivity.kt
+++ Kotlin_Kinopoisk/app/src/main/java/com/example/kotlin_kinopoisk/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val text = findViewById<TextView>(R.id.textview)
         text.setText("layout")
-
+        //efeefe
     }
 }
